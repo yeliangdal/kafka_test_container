27e49c644fa5cd56339a95a4784468acd34d1629
yl <liangyens2012@gmail.com>
2024-11-16 13:02:40 -0400

    add playground

---
+++ src/main/java/kafkaTestContainer/playground/playground.java
@@ -0,0 +1,21 @@
+package kafkaTestContainer.playground;
+
+import java.util.Comparator;
+
+public class playground {
+
+    
+    public static short countBits(int x) {
+        short numOfBits = 0;
+        while (x != 0) {
+            numOfBits += x&1;
+            x >>>= 1;
+        }
+        return numOfBits;
+    }
+
+    public static void main(String[] args) {
+        Character chars = 
+
+    }
+}
